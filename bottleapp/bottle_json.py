45158c569107e40cb62b56019eecef52ca819d89
samyuktahegde <hegdesamyukta711@gmail.com>
2018-06-06 10:18:00 +0530

    Python Bottle Framework Tryouts

--- bottleapp/bottle_json.py
+++ bottleapp/bottle_json.py
@@ -3,6 +3,7 @@ from bottle import run, route
 @route('/')
 def index():
     # return '<h1>HTML</h1>'
-    return {'name':'jsonData', 'myList':[1,2,3,4,5]}
+    # return {'name':'jsonData', 'myList':[1,2,3,4,5]}
+    return 'Hello'
 
 run(debug=True, reloader=True)
